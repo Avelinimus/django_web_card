4ca7dc2a70629196c3556a11dc93c4d0ae353f6e
avelinimus <avel63146@gmail.com>
2018-07-05 20:47:04 +0300

    Index page

--- django_web_card/urls.py
+++ django_web_card/urls.py
@@ -13,9 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('main_app.urls', 'main_app'), namespace='main_app')),
 ]
+
+urlpatterns += [
+    # Добавьте URL соотношения, чтобы перенаправить запросы с корневового URL, на URL приложения
+    path('', RedirectView.as_view(url='', permanent=True)),
+]
+
+urlpatterns += [
+    # Подключаем интерактивный редактор
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+urlpatterns = [
+    path('index', views.index, name='index'),
+]
