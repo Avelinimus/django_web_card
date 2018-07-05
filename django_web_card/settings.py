8a88ba007a537f4fc519857cc151e8c0630e1ebe
avelinimus <avel63146@gmail.com>
2018-07-05 20:14:10 +0300

    Index page

--- django_web_card/settings.py
+++ django_web_card/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+# Привет
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -110,9 +110,9 @@ CKEDITOR_CONFIGS = {
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'e'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
