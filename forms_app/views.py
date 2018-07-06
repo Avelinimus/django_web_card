07a3b63ba1c725455bc0ec4b4f4c8b1b638f6cce
Limb <alexandrmaksimchuck@gmail.com>
2018-07-06 16:55:37 +0300

    settings

---
+++ forms_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from .forms import SupportForm
+
+
+def send_supp(request):
+    if request.method != 'POST':
+        form = SupportForm()
+    else:
+        form = SupportForm(data=request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']  # проверенные данные отправлены в переменную name
+            email = form.cleaned_data['email']
+            comment = form.cleaned_data['comment']
+            # Текст ,отправляемый нам на почту
+            data = 'Имя: {0},\n\n Проблема: {1},\n\n E-mail: {2}'.format(name, comment, email)
+            send_mail('Support django_web_card', data, name, ['developerspython385@gmail.com'],
+                      fail_silently=False)
+            first_context = {'name': name}
+            return render(request, 'forms_app/thank_for_support.html', first_context)
+    context = {'form': form}
+    return render(request, 'forms_app/support.html', context)
